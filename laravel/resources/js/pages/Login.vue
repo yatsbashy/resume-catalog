56b8e500804eedbe51ae79303fba35764272e942
yatsubashi <gakuyabushita@gmail.com>
2019-10-15 16:36:09 +0900

    Create Login & Register form

--- laravel/resources/js/pages/Login.vue
+++ laravel/resources/js/pages/Login.vue
@@ -1,3 +1,67 @@
 <template>
-  <h1>Login</h1>
+  <div class="container--small">
+    <ul class="tab">
+      <li class="tab__item" :class="{'tab__item--active': tab === 1 }" @click="tab = 1">ログイン</li>
+      <li class="tab__item" :class="{'tab__item--active': tab === 2 }" @click="tab = 2">新規登録</li>
+    </ul>
+    <div class="panel" v-show="tab === 1">
+      <form class="form" @submit.prevent="login">
+        <label for="login-email">メールアドレス</label>
+        <input type="text" class="form__item" id="login-email" v-model="loginForm.email" />
+        <label for="login-password">パスワード</label>
+        <input type="password" class="form__item" id="login-password" v-model="loginForm.password" />
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">ログイン</button>
+        </div>
+      </form>
+    </div>
+    <div class="panel" v-show="tab === 2">
+      <form class="form" @submit.prevent="register">
+        <label for="username">名前</label>
+        <input type="text" class="form__item" id="username" v-model="registerForm.name" />
+        <label for="email">メールアドレス</label>
+        <input type="text" class="form__item" id="email" v-model="registerForm.email" />
+        <label for="password">パスワード</label>
+        <input type="password" class="form__item" id="password" v-model="registerForm.password" />
+        <label for="password-confirmation">パスワード (確認)</label>
+        <input
+          type="password"
+          class="form__item"
+          id="password-confirmation"
+          v-model="registerForm.password_confirmation"
+        />
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">新規登録</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tab: 1,
+      loginForm: {
+        email: '',
+        password: ''
+      },
+      registerForm: {
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+      }
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.loginForm);
+    },
+    register() {
+      console.log(this.registerForm);
+    }
+  }
+};
+</script>
