9d92894e0f19e3d17c519669eed319483602c74b
yatsubashi <gakuyabushita@gmail.com>
2019-10-17 12:12:31 +0900

    Add calling Register, Login & Logout API

--- laravel/resources/js/components/Footer.vue
+++ laravel/resources/js/components/Footer.vue
@@ -1,6 +1,17 @@
 <template>
   <footer class="footer">
-    <button class="button button--link">ログアウト</button>
+    <button class="button button--link" @click="logout">ログアウト</button>
     <RouterLink class="button button--link" to="/login">ログイン / 新規登録</RouterLink>
   </footer>
 </template>
+
+<script>
+export default {
+  methods: {
+    async logout() {
+      await this.$store.dispatch('auth/logout');
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
