e13189f4d9fa910491bf04c8a7d988234521b0ac
yatsubashi <gakuyabushita@gmail.com>
2019-10-21 18:05:48 +0900

    Fix UI: Get current user at startup

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -4,10 +4,16 @@ import router from './router';
 import store from './store';
 import App from './App.vue';
 
-const app = new Vue({
-  el: '#app',
-  router,
-  store,
-  components: { App },
-  template: '<App />'
-});
+const createVm = async () => {
+  await store.dispatch('auth/currentUser');
+
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App />'
+  });
+};
+
+createVm();
