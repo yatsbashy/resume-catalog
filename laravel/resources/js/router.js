2d80ebb12b6deee6595f35b746bac430c5f9b8db
yatsubashi <gakuyabushita@gmail.com>
2019-11-03 15:21:34 +0900

    Create UI: ResumeDetail page

--- laravel/resources/js/router.js
+++ laravel/resources/js/router.js
@@ -4,6 +4,10 @@ import store from './store';
 
 // Components
 import ResumeList from './pages/ResumeList.vue';
+import ResumeDetail from './pages/ResumeDetail.vue';
+import Profile from './components/Profile.vue';
+import Skills from './components/Skills.vue';
+import Works from './components/Works.vue';
 import Login from './pages/Login.vue';
 import NotFound from './pages/errors/NotFound.vue';
 import SystemError from './pages/errors/System.vue';
@@ -15,6 +19,29 @@ const routes = [
     path: '/',
     component: ResumeList,
     meta: { requiresAuth: true }
+  },
+  {
+    path: '/users/:id',
+    component: ResumeDetail,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: '',
+        redirect: 'profile'
+      },
+      {
+        path: 'profile',
+        component: Profile
+      },
+      {
+        path: 'skills',
+        component: Skills
+      },
+      {
+        path: 'works',
+        component: Works
+      }
+    ]
   },
   {
     path: '/login',
